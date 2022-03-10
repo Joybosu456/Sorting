98c87195c977381132f42a9a3a51fc4e73817bb9
JOY <joybosu@gmail.com>
2022-03-10 22:04:00 +0530

    Joy push Buble sort program

---
+++ BubleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array=new int[5] {40,57,20,9,8};
+            int k;
+            int temp;
+            for(int i=0;i<array.Length;i++)
+            {
+                k = i - 1;
+                while(k>=0 && array[k]>array[k+1])
+                {
+                    temp = array[k];
+                    array[k] = array[k+1];
+                    array[k+1] = temp;
+                    k = k - 1;
+                }
+            }
+
+            for(int i=0; i<array.Length;i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
